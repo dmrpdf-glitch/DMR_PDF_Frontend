227bac9e7911863f4bbbfc75af6523534781d3a2
Keerthucreations <mohanofficail1612@gmail.com>
2026-05-04 13:08:13 +0530

    fix: remove pdf sidebar and toolbar

--- src/DrawingViewerPage.js
+++ src/DrawingViewerPage.js
@@ -629,7 +629,7 @@ const groupData = () => {
 <iframe
   src={`https://mozilla.github.io/pdf.js/web/viewer.html?file=${encodeURIComponent(
     selectedDrawing.fileUrl
-  )}#navpanes=0&toolbar=1`}
+  )}&sidebar=0#toolbar=0`}
   title={selectedDrawing.drawingName}
   className="pdf-frame"
   style={{
